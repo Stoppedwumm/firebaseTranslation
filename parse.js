61fd5be477515d5dee08534f1d320dc74fb5034c
stoppedwumm <mobileboxnews@icloud.com>
2025-01-22 15:32:22 +0100

    Add delete statement

--- parse.js
+++ parse.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 let config = require("./firebase-js-sdk/lerna.json")
 
-config.useWorkspaces = false
+// remove useWorkspaces
+delete config["useWorkspaces"]
 
 fs.writeFileSync("./firebase-js-sdk/lerna.json", JSON.stringify(config, null, 2))
